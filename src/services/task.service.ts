b8e58dd7e64e0cc93f0e53faaa6619d996daad1d
nasc1mento <anr@discente.ifpe.edu.br>
2024-05-18 21:19:35 -0300

    feat: service | create task service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,5 +1,30 @@
+import { TaskRepository } from "../repository/task/task.repository";
 
 
 export class TaskService {
-    
+    private repository: TaskRepository;
+
+    constructor() {
+        this.repository = new TaskRepository;
+    }
+
+    createTask(tag: ITag) :Promise<ITag> {
+        return this.repository.save(tag);
+    }
+
+    getTaskById(id: string): Promise<ITag> {
+        return this.repository.get(id);
+    }
+
+    getAllTasks(): Promise<ITag[]> {
+        return this.repository.getAll();
+    }
+
+    updateTask(id: string, tag): Promise<ITag> {
+        return this.repository.update(id, tag);
+    }
+
+    deleteById(id: string): Promise<ITag> {
+        return this.repository.delete(id);
+    }
 }
