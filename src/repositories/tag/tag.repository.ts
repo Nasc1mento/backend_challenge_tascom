19cd74cd6a1c761bfc7c3b6a013aa3ffe9dfadbb
nasc1mento <anr@discente.ifpe.edu.br>
2024-05-21 01:51:36 -0300

    style: repositories | removing unnecessary const instances

--- src/repositories/tag/tag.repository.ts
+++ src/repositories/tag/tag.repository.ts
@@ -12,59 +12,49 @@ export class TagRepository implements ITagRepository {
     }
 
     async save(tag: ITag): Promise<ITag> {
-        const newtag =  new Promise<ITag>(async(resolve, reject) => {
+        return new Promise<ITag>(async(resolve, reject) => {
             await this.model.create(new TagEntity(tag))
             .then((tag) => {
                 resolve(tag);
             }).catch((err) => reject(err));
         });
-
-        return newtag;
     }
 
     async getAll(): Promise<ITag[]> {
-        const collectedTags = new Promise<ITag[]>(async(resolve, reject) => {
+        return new Promise<ITag[]>(async(resolve, reject) => {
             await this.model.find().then((tags) => {
                 resolve(tags);
             }).catch((error) => reject(error));
         });
-
-        return collectedTags;
     }
 
     async update(id: string, tag: ITag): Promise<ITag> {
-        const tagUpdated = new Promise<ITag>(async(resolve, reject) => {
+        return new Promise<ITag>(async(resolve, reject) => {
             await this.model.findByIdAndUpdate(id, tag, {new: true}).then((tag) => {
                 resolve(tag);
             }).catch((error) => {
                 reject(error);
             })
         });
-
-        return tagUpdated;
     }
 
     async delete(id: string): Promise<ITag> {
-        const deletedTag = new Promise<ITag>(async(resolve, reject) => {
+        return new Promise<ITag>(async(resolve, reject) => {
             await this.model.findByIdAndDelete(id).then((tag) => {
                 resolve(tag);
             }).catch((err) => {
                 reject(err);
             })
         });
-
-        return deletedTag;
     }
 
     async get(id: string): Promise<ITag> {
-        const tagCollected = new Promise<ITag>(async(resolve, reject) => {
+        return new Promise<ITag>(async(resolve, reject) => {
             await this.model.findById(id).then((task) => {
                 resolve(task);
             }).catch((error) => {
                 reject(error);
             })
         });
-
-        return tagCollected;
     }
 }
--- src/repositories/task/task.repository.ts
+++ src/repositories/task/task.repository.ts
@@ -13,94 +13,83 @@ export class TaskRepository implements ITaskRepository {
     }
 
     async save(task: ITask): Promise<ITask> {
-        const newTask = new Promise<ITask>(async(resolve, reject) => {
+        return new Promise<ITask>(async(resolve, reject) => {
             await this.model.create(new TaskEntity(task)).then((task) => {
                 resolve(task);
             }).catch((error) => {
                 reject(error);
             });
         });
-        return newTask;
     }
 
     async getAll(): Promise<ITask[]> {
-        const tasksCollected = new Promise<ITask[]>(async(resolve, reject) => {
+        return new Promise<ITask[]>(async(resolve, reject) => {
             await this.model.find().then((tasks) => {
                 resolve(tasks);
             }).catch((error) => {
                 reject(error);
             });
         });
-        return tasksCollected;
     }
 
     async update(id: string, task: ITask): Promise<ITask> {
-        const updatedTask = new Promise<ITask>(async (resolve, reject) => {
+        return new Promise<ITask>(async (resolve, reject) => {
             await this.model.findByIdAndUpdate(id, task, {new: true}).then((task) => {
                     resolve(task);
             }).catch((error) => {
                 reject(error);
             });
         });
-
-        return updatedTask;
     }
 
     async delete(id: string): Promise<ITask> {
-        const deletedTag = new Promise<ITask>(async(resolve, reject) => {
+        return new Promise<ITask>(async(resolve, reject) => {
             this.model.findByIdAndDelete(id).then((task) => {
                 resolve(task)
             }).catch((error) => {
                 reject(error);
             })
         });
-
-        return deletedTag;
     }
 
     async get(id: string): Promise<ITask> {
-        const taskCollected = new Promise<ITask>(async(resolve, reject) => {
+        return new Promise<ITask>(async(resolve, reject) => {
             await this.model.findById(id).then((task) => {
                 resolve(task);
             }).catch((error) => {
                 reject(error);
             });
         });
-
-        return taskCollected;
     }
 
     async addTag(taskId: string, tagId: string): Promise<ITask> {
-        const task = new Promise<ITask>(async(resolve, reject) => {
+        return new Promise<ITask>(async(resolve, reject) => {
             await this.model.findByIdAndUpdate(taskId, {$addToSet: {tags: tagId}}, {new: true}).then((task) => {
                 resolve(task);
             }).catch((error) => {
                 reject(error);
             })
         });
-        return task;
     }
 
     async removeTag(taskId: string, tagId: string): Promise<ITask> {
-        const task = new Promise<ITask>(async(resolve, reject) => {
+        return new Promise<ITask>(async(resolve, reject) => {
             await this.model.findByIdAndUpdate(taskId, {$pull: {tags: tagId}}, {new: true}).then((task) => {
                 resolve(task);
             }).catch((error) => {
                 reject(error);
             });
         });
-        return task;
     }
     
     async getAllByTags(tagIds: string[]): Promise<ITask[]> {
-        const tasks = new Promise<ITask[]>(async(resolve, reject) => {
+        return new Promise<ITask[]>(async(resolve, reject) => {
             await this.model.find({tags: {$in: tagIds}}).then((tasks) => {
                 resolve(tasks);
             }).catch((error) => {
                 reject(error);
             });
         });
-        return tasks;
     }
 
     async removeTagFromAllTasks(tagId: string): Promise<any> {
